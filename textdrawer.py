46cb1018c050f72a30a1e0f8ef624c79d72a37d8
Adam <michna_a@o2.pl>
2013-03-23 00:13:38 +0100

    prosta implemętacja drukowania czcionek

--- textdrawer.py
+++ textdrawer.py
@@ -2,5 +2,7 @@ class TextDrawer:
     def setFont(self, font):
 	self.font = font
 
-    def draw(self, text):
+     def draw(self, text):
+        for character in text:
+            print self.font.letters[character.upper()]
         pass
